f5f2616ea827b77533e3464f49bb2e5596850403
restaurantlembouchure-dev <restaurant.lembouchure@gmail.com>
2026-03-30 14:12:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   RESTAURANT_NAME: "L'Embouchure",
-  APPS_SCRIPT_URL: "REMPLACE_PAR_TON_URL_EXEC",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzbercXLtx0hpyZbw_UcFUeIiMSbzbE6G3CqtrpIErJNoylmeOIkAAGOJ4S0Su021m9jA/exec",
   USERS: [
     { name: "Christelle", pin: "1111" },
     { name: "Ludovic", pin: "2222" },
